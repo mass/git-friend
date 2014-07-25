1dbe319da208e96b9a64be565628d893a579621c
Andrew Mass <amass1212@gmail.com>
2014-07-25 01:13:22 -0700

    Update styling and add some GitHub api logic.

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 angular.module('git-friend', [])
   .controller('GitFriendController', function($scope, $http) {
 
-  $scope.user = {};
+    $scope.user = {};
 
-  $http.get('https://api.github.com/users/mass')
-    .success(function(data) {
-      console.log(data);
-      angular.extend($scope.user, _.pick(data, ['avatar_url', 'name', 'login']));
-    });
+    $scope.loadData = function(username) {
+      $http.get('https://api.github.com/users/' + username)
+        .success(function(data) {
+          console.log(data);
+          angular.extend($scope.user, _.pick(data, ['avatar_url', 'name', 'login']));
+        });
 
-  $http.get('https://api.github.com/users/mass/starred')
-    .success(function(data) {
-      console.log(data);
+      $http.get('https://api.github.com/users/' + username + '/starred')
+        .success(function(data) {
+          console.log(data);
 
-      $scope.user.stars = _.map(data, function(repo) {
-        repo = _.pick(repo, ['full_name', 'html_url']);
-        return repo;
-      });
-    });
-});
+          $scope.user.stars = _.map(data, function(repo) {
+            repo = _.pick(repo, ['full_name', 'html_url', 'stargazers_url']);
+
+            console.log(repo.stargazers_url);
+
+            $http.get(repo.stargazers_url)
+              .success(function(data) {
+                console.log(data);
+                console.log('hi');
+              });
+
+            return repo;
+          });
+        });
+    }
+  });
