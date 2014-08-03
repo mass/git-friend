57456de7bf6d2ece9993419a520065e96014a73c
Andrew Mass <amass1212@gmail.com>
2014-08-02 18:12:32 -0700

    Support pagination of API requests. Improve error handling.

--- client/app.js
+++ client/app.js
@@ -4,14 +4,23 @@ angular.module('git-friend', [])
     $scope.user = {};
 
     $scope.loadData = function(username) {
+      $scope.user = {};
+      $scope.error = null;
+
       $http.get('/user/' + username)
         .success(function(data) {
           angular.extend($scope.user, data);
+        })
+        .error(function(data) {
+          $scope.error = data.error;
         });
 
       $http.get('/stars/' + username)
         .success(function(data) {
           $scope.user.stars = data;
+        })
+        .error(function(data) {
+          $scope.error = data.error;
         });
     }
   });
--- server/server.js
+++ server/server.js
@@ -16,32 +16,61 @@ app.get('/', function(request, response) {
 app.get('/user/:username', function(request, response) {
   fetchData('/users/' + request.params.username, function(result) {
     if (result) {
-      if (result.status == 200) {
+      if (result.status === 200) {
         result.body = _.pick(result.body, "avatar_url", "login", "name");
         return response.send(result.body);
+      } else if (result.status === 403 && result.headers['x-ratelimit-remaining'] === '0') {
+        response.status(result.status);
+        response.send({'status': result.status, 'error': 'rate-limit-exceeded'});
+      } else {
+        response.status(result.status);
+        response.send({'status': result.status, 'error': 'internal-server-error'})
       }
-      response.status(result.status).end();
     } else {
-      response.status(500).end();
+      response.status(500);
+      response.send({'status': 500, 'error': 'internal-server-error'})
     }
   });
 });
 
 app.get('/stars/:username', function(request, response) {
-  fetchData('/users/' + request.params.username + '/starred', function(result) {
-    if (result) {
-      if (result.status == 200) {
-        result.body = _.map(result.body, function(repo) {
-          return _.pick(repo, 'full_name', 'html_url', 'stargazers_url', 'stargazers_count');
-        });
-        return response.send(result.body);
-      }
-      response.status(result.status).end();
-    } else {
-      response.status(500).end();
-    }
+  var stars = [];
+  getStarredPage(stars, 1, request.params.username, 1, response, function(stars) {
+    response.send(stars);
   });
 });
+
+var getStarredPage = function(stars, pages, username, page, response, successCallback) {
+  fetchData('/users/' + username + '/starred?page=' + page, function(result) {
+    if (result) {
+      if (result.status == 200) {
+        if (result.headers.link) {
+          var links = result.headers.link.replace(/ /g, '');
+          pages = links.substr(links.indexOf('rel="last"') - 3, 1);
+        }
+
+        stars = stars.concat(_.map(result.body, function(repo) {
+          return _.pick(repo, 'full_name', 'html_url', 'stargazers_url', 'stargazers_count');
+        }));
+
+        if (page >= pages) {
+          return successCallback(stars);
+        } else {
+          getStarredPage(stars, pages, username, page + 1, response, successCallback);
+        }
+      } else if (result.status === 403 && result.headers['x-ratelimit-remaining'] === '0') {
+        response.status(result.status);
+        response.send({'status': result.status, 'error': 'rate-limit-exceeded'});
+      } else {
+        response.status(result.status);
+        response.send({'status': result.status, 'error': 'internal-server-error'})
+      }
+    } else {
+      response.status(500);
+      response.send({'status': 500, 'error': 'internal-server-error'})
+    }
+  });
+};
 
 var fetchData = function(path, callback) {
   var options = {
